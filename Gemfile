c94f89e72b621659b714903414db92a925af5143
nalabjp <nalabjp@gmail.com>
2014-09-08 01:45:35 +0900

    Implemented trello to chatwork base

--- Gemfile
+++ Gemfile
@@ -1,9 +1,13 @@
 # A sample Gemfile
 source "https://rubygems.org"
 
+gem 'thor'
+gem 'activesupport'
 gem 'sinatra'
 gem 'sinatra-contrib'
 gem 'thin'
 gem 'foreman'
 gem 'ruby-trello'
 gem 'slack-notifier'
+
+gem 'pry',  group: :development
---
+++ app.rb
@@ -0,0 +1,31 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'json'
+require "#{File.expand_path(File.dirname(__FILE__))}/hooks"
+require "#{File.expand_path(File.dirname(__FILE__))}/notifiers"
+
+set :server, 'thin'
+
+# create webhook
+hooks = Hooks.new
+hooks.create
+
+# notifiers
+notifiers = Notifiers.new(hooks.routes)
+
+get '/' do
+  'trello to chatwork'
+end
+
+post '/cb' do
+  json = JSON.parse(request.body.read)
+  notifiers.notify(json)
+end
+
+# delete webhook
+# Require before `run Sinatra::Application`
+at_exit do
+  p 'at_exit start'
+  hooks.delete
+  p 'at_exit end'
+end
