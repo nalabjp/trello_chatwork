0eb9a5c56ab7c663d3d739b5a839a9898b985fe3
nalabjp <nalabjp@gmail.com>
2014-09-08 09:25:50 +0900

    Fixed hook start

--- app.rb
+++ app.rb
@@ -1,14 +1,9 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'json'
-require "#{File.expand_path(File.dirname(__FILE__))}/hooks"
 require "#{File.expand_path(File.dirname(__FILE__))}/notifiers"
 
 set :server, 'thin'
-
-# create webhook
-hooks = Hooks.new
-hooks.create
 
 # notifiers
 notifiers = Notifiers.new(hooks.routes)
@@ -24,12 +19,4 @@ end
 
 head '/cb' do
   'for webhook'
-end
-
-# delete webhook
-# Require before `run Sinatra::Application`
-at_exit do
-  p 'at_exit start'
-  hooks.delete
-  p 'at_exit end'
 end
--- config.ru
+++ config.ru
@@ -1,2 +1,15 @@
 require "#{File.expand_path(File.dirname(__FILE__))}/app"
+require "#{File.expand_path(File.dirname(__FILE__))}/hooks"
 run Sinatra::Appliction
+
+# create webhook
+hooks = Hooks.new
+hooks.create
+
+# delete webhook
+# Require before `run Sinatra::Application`
+at_exit do
+  p 'at_exit start'
+  hooks.delete
+  p 'at_exit end'
+end
