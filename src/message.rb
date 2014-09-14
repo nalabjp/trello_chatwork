3fabb1bdb7858d2aa48b81f8c3f334c43341126d
nalabjp <nalabjp@gmail.com>
2014-09-15 03:53:13 +0900

    Refactored

--- src/message.rb
+++ src/message.rb
@@ -1,60 +1,60 @@
 module Message
   class Card
     class << self
-      def create(full_name, card_name, list_name)
-        "'#{full_name}'が'#{card_name}'カードを'#{list_name}'リストに作成しました"
+      def create(creator, card, list)
+        "'#{creator}'が'#{card}'カードを'#{list}'リストに作成しました"
       end
 
-      def comment(full_name, card_name, list_name, data_text)
-        "'#{full_name}'が'#{list_name}'リストの'#{card_name}'カードに'#{data_text}'とコメントしました"
+      def comment(creator, card, list, text)
+        "'#{creator}'が'#{list}'リストの'#{card}'カードに'#{text}'とコメントしました"
       end
 
-      def move(full_name, card_name, list_before, list_after)
-        "'#{full_name}'が'#{card_name}'カードを'#{list_before}'リストから'#{list_after}'リストに移動しました"
+      def move(creator, card, before, after)
+        "'#{creator}'が'#{card}'カードを'#{before}'リストから'#{after}'リストに移動しました"
       end
 
-      def up_position(full_name, card_name, list_name)
-        "'#{full_name}'が'#{list_name}'リストの'#{card_name}'カードの優先度を上げました"
+      def up_position(creator, card, list)
+        "'#{creator}'が'#{list}'リストの'#{card}'カードの優先度を上げました"
       end
 
-      def down_position(full_name, card_name, list_name)
-        "'#{full_name}'が'#{list_name}'リストの'#{card_name}'カードの優先度を下げました"
+      def down_position(creator, card, list)
+        "'#{creator}'が'#{list}'リストの'#{card}'カードの優先度を下げました"
       end
 
-      def archive(full_name, card_name, list_name)
-        "'#{full_name}'が'#{list_name}'リストの'#{card_name}'カードをアーカイブしました"
+      def archive(creator, card, list)
+        "'#{creator}'が'#{list}'リストの'#{card}'カードをアーカイブしました"
       end
 
-      def unarchive(full_name, card_name, list_name)
-        "'#{full_name}'がアーカイブしていた'#{card_name}'カードを'#{list_name}'リストに戻しました"
+      def unarchive(creator, card, list)
+        "'#{creator}'がアーカイブしていた'#{card}'カードを'#{list}'リストに戻しました"
       end
 
-      def add_member(creator_name, card_name, member_name)
-        "'#{creator_name}'が'#{member_name}'を'#{card_name}'カードのメンバーに追加しました"
+      def add_member(creator, card, member)
+        "'#{creator}'が'#{member}'を'#{card}'カードのメンバーに追加しました"
       end
 
-      def remove_member(creator_name, card_name, member_name)
-        "'#{creator_name}'が'#{member_name}'を'#{card_name}'カードのメンバーから削除しました"
+      def remove_member(creator, card, member)
+        "'#{creator}'が'#{member}'を'#{card}'カードのメンバーから削除しました"
       end
 
-      def add_label(creator_name, card_name, label_name)
-        "'#{creator_name}'が'#{card_name}'カードに'#{label_name}'ラベルを追加しました"
+      def add_label(creator, card, label)
+        "'#{creator}'が'#{card}'カードに'#{label}'ラベルを追加しました"
       end
 
-      def remove_label(creator_name, card_name, label_name)
-        "'#{creator_name}'が'#{card_name}'カードの'#{label_name}'ラベルを削除しました"
+      def remove_label(creator, card, label)
+        "'#{creator}'が'#{card}'カードの'#{label}'ラベルを削除しました"
       end
     end
   end
 
   class List
     class << self
-      def archive(full_name, list_name)
-        "'#{full_name}'が'#{list_name}'リストをアーカイブしました"
+      def archive(creator, list)
+        "'#{creator}'が'#{list}'リストをアーカイブしました"
       end
 
-      def unarchive(full_name, list_name)
-        "'#{full_name}'がアーカイブしていた'#{list_name}'リストを戻しました"
+      def unarchive(creator, list)
+        "'#{creator}'がアーカイブしていた'#{list}'リストを戻しました"
       end
     end
   end
